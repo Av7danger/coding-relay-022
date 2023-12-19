1281e3194e8d20594d1d70efe4932d822161323a
D Anish Varma <142290675+Av7danger@users.noreply.github.com>
2023-12-19 16:47:56 +0530

    third commit

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-// Sample song data
-const songs = [
-  {
-    title: 'Song 1',
-    artist: 'Artist 1',
-    file: "https://www.youtube.com/watch?v=-tJYN-eG1zk"
-  },
-  {
-    title: 'Song 2',
-    artist: 'Artist 2',
-    file: "https://www.youtube.com/watch?v=-tJYN-eG1zk"
-  },
-  // Add more songs with their details
-];
 
-// Function to create playlist
-function createPlaylist() {
-  const playlist = document.getElementById('playlist-list');
-  songs.forEach((song, index) => {
-    const listItem = document.createElement('a');
-    listItem.href = '#';
-    listItem.className = 'list-group-item list-group-item-action';
-    listItem.innerText = `${index + 1}. ${song.title} - ${song.artist}`;
-    listItem.addEventListener('click', () => playSong(index));
-    playlist.appendChild(listItem);
-  });
-}
+    // Sample song data
+    const songs = [
+      {
+        title: 'Song 1',
+        artist: 'Artist 1',
+        file: 'your-audio-file1.mp3' // Replace with the actual audio file URL
+      },
+      {
+        title: 'Song 2',
+        artist: 'Artist 2',
+        file: 'your-audio-file2.mp3' // Replace with the actual audio file URL
+      },
+      // Add more songs with their details
+    ];
 
-// Function to play selected song
-function playSong(index) {
-  const audio = document.getElementById('audio-player');
-  audio.src = songs[index].file;
-  audio.play();
-}
+    let currentSongIndex = 0;
+    const audio = document.getElementById('audio-player');
 
-// Initialize the playlist
-window.onload = createPlaylist;
-// Define addSong function
-function addSong(song) {
-  // Create songCard string with song data
-  const songCard = `
-     <div class="col-md-4 mb-4">
-       <div class="card">
-         <img src="${song.cover}" class="card-img-top" alt="${song.title}">
-         <div class="card-body">
-           <h5 class="card-title">${song.title}</h5>
-           <p class="card-text">${song.artist}</p>
-         </div>
-       </div>
-     </div>
-  `;
+    // Function to play/pause the audio
+    function playPause() {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    }
 
-  // Append songCard to music-library
-  $('#music-library').append(songCard);
-} 
+    // Function to play the previous song
+    function previousSong() {
+      currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+      playSelectedSong(currentSongIndex);
+    }
+
+    // Function to play the next song
+    function nextSong() {
+      currentSongIndex = (currentSongIndex + 1) % songs.length;
+      playSelectedSong(currentSongIndex);
+    }
+
+    // Function to play the selected song
+    function playSelectedSong(index) {
+      audio.src = songs[index].file;
+      audio.play();
+    }
+
+    // Initialize the first song
+    playSelectedSong(currentSongIndex);
